9adc5da41474300c20edcb8db4f11db1c9eeacfe
Miguel ubuntu <miguel-dev@dropstudio.mx>
2023-06-11 14:44:38 -0600

    started applayout design

---
+++ resources/js/Components/MyComponents/SideNav.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- sidebar -->
+    <div class="h-screen hidden lg:block shadow-lg relative">
+        <div class="bg-[#D9D9D9] h-full">
+            <nav class="pt-14">
+                <div>
+                    <div class="mb-16 items-center font-semibold text-[#0355B5] flex flex-col">
+                        <span>Work</span>
+                        <span>01:37:27</span>
+                    </div>
+                    <template v-for="(menu, index) in menus" :key="index">
+                        <SideNavLink :href="menu.route" :active="menu.active">
+                            <span v-html="menu.icon"></span>
+                            <span class="text-sm font-normal">
+                                {{ menu.label }}
+                            </span>
+                        </SideNavLink>
+                    </template>
+                </div>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import SideNavLink from "@/Components/MyComponents/SideNavLink.vue";
+
+export default {
+    data() {
+        return {
+            menus: [
+                {
+                    label: 'Inicio',
+                    icon: '<i class="fa-solid fa-house"></i>',
+                    route: route('dashboard'),
+                    active: route().current('dashboard'),
+                    // show: this.$page.props.auth.user.is_admin
+                },
+                {
+                    label: 'Catálogo',
+                    icon: '<i class="fa-solid fa-book-open"></i>',
+                    route: route('dashboard'),
+                    active: route().current('dashboard'),
+                    // show: this.$page.props.auth.user.is_admin
+                },
+            ],
+        }
+    },
+    components: {
+        SideNavLink,
+    }
+}
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        {{-- font awesome --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Scripts -->
         @routes
