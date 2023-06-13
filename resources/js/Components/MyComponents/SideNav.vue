276c22f35c996f36a8d33e6b49d3ce30bba00647
MiguelVR <miguelvz26.mv@gmail.com>
2023-06-13 12:13:18 -0600

    created quote route resource

--- resources/js/Components/MyComponents/SideNav.vue
+++ resources/js/Components/MyComponents/SideNav.vue
@@ -64,7 +64,7 @@ export default {
                     options: [
                         {
                             label: 'Cotizaciones',
-                            route: 'dashboard'
+                            route: 'quotes.index'
                         },
                         {
                             label: 'Clientes',
