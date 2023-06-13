b8440f8a35e8ecc22ed1ec5282e048c90e3a59c9
Angel <angelvazquez470@gmail.com>
2023-06-13 11:09:49 -0600

    instalacion de element plus y configuración

--- app/Http/Controllers/CatalogProductController.php
+++ app/Http/Controllers/CatalogProductController.php
@@ -7,57 +7,43 @@ use Illuminate\Http\Request;
 
 class CatalogProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+        return inertia('CatalogProduct/Index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(CatalogProduct $catalogProduct)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+    
     public function edit(CatalogProduct $catalogProduct)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    
     public function update(Request $request, CatalogProduct $catalogProduct)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    
     public function destroy(CatalogProduct $catalogProduct)
     {
         //
--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,9 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +18,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(ElementPlus)
             .mount(el);
     },
     progress: {
