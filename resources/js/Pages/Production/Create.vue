8fba3330b4328907fe8b4194146ffbabcb38722e
Miguel Vázquez <miguelvz26.mv@gmail.com>
2023-09-20 11:38:18 -0600

    decrement stock automatically when production is created

--- resources/js/Pages/Production/Create.vue
+++ resources/js/Pages/Production/Create.vue
@@ -255,7 +255,7 @@ export default {
         onSuccess: () => {
           this.$notify({
             title: "Éxito",
-            message: "Órdenes de producción creadas",
+            message: "Órdenes de producción creadas. Se han descontado las cantidades del stock automáticamente",
             type: "success",
           });
 
