fa73567f4171e84e402baab7e797cca26b042eda
Angel Vazquez <angelvazquez470@gmail.com>
2023-10-01 17:49:42 -0600

    cronograma finished

--- resources/js/Components/MyComponents/EmptyTable.vue
+++ resources/js/Components/MyComponents/EmptyTable.vue
@@ -2,7 +2,7 @@
     <div class="border-blue-500">
         <div class=" container mx-auto my-4 border-green-600">
          <el-table-v2
-         class="border-amber-700"
+         class="border-amber-700 bg-[#9A9A9A]"
             :columns="columns"
             :data="[]"
             :row-height="40"
@@ -11,7 +11,7 @@
             :footer-height="50"
         >
             <template #empty>
-            <div class="flex items-center justify-center h-100% border-amber-500 text-amber-700">
+            <div class="flex items-center justify-center h-100% border-amber-500 text-amber-700 bg-[#0355B5] bg-green-500">
                 <el-empty />
             </div>
             </template>
