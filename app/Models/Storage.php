df0e7a9977423edf28628a9f05a1b9b3fbef3810
Miguel Vázquez <miguelvz26.mv@gmail.com>
2023-11-15 19:09:49 -0600

    some

--- app/Models/Storage.php
+++ app/Models/Storage.php
@@ -29,9 +29,8 @@ class Storage extends Model
 
     public static function lowStock()
     {
-        return collect([]);
         return self::where('type', 'materia-prima')->whereHas('storageable', function ($query) {
-            $query->whereColumn('storages.quantity', '<=', 'raw_materials.min_quantity');
+            $query->whereColumn('storages.quantity', '<=', 'min_quantity');
         })->with('storageable')->get();
     }
 
