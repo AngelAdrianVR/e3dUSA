d1e8380384fd1e33fff54a48a42b58390c2fde9c
Angel Vazquez <angelvazquez470@gmail.com>
2023-11-20 18:36:39 -0600

    optimized client_monitors' index

--- app/Http/Controllers/ClientMonitorController.php
+++ app/Http/Controllers/ClientMonitorController.php
@@ -16,7 +16,28 @@ class ClientMonitorController extends Controller
    
     public function index()
     {
-        $client_monitors = ClientMonitorResource::collection(ClientMonitor::with('company', 'seller', 'oportunity', 'emailMonitor', 'paymentMonitor', 'mettingMonitor', 'whatsappMonitor')->latest()->get());
+        // $client_monitors = ClientMonitorResource::collection(ClientMonitor::with('company', 'seller', 'oportunity', 'emailMonitor', 'paymentMonitor', 'mettingMonitor', 'whatsappMonitor')->latest()->get());
+        $pre_client_monitors = ClientMonitorResource::collection(ClientMonitor::with('company', 'seller', 'oportunity', 'emailMonitor', 'paymentMonitor', 'mettingMonitor', 'whatsappMonitor')->latest()->get());
+        $client_monitors = $pre_client_monitors->map(function ($client_monitor) {
+            if ($client_monitor->oportunity) {
+                $folio = 'S-' . strtoupper(substr($client_monitor->oportunity?->name, 0, 3)) . '-' . strtoupper(substr($client_monitor->type, 0, 1)) . str_pad($client_monitor->id, 4, '0', STR_PAD_LEFT);
+            } else {
+                $folio = 'S-' . strtoupper(substr($client_monitor->company?->business_name, 0, 3)) . '-' . strtoupper(substr($client_monitor->type, 0, 1)) . str_pad($client_monitor->id, 4, '0', STR_PAD_LEFT);
+            }
+            return [
+                'id' => $client_monitor->id,    
+                'folio' => $folio,
+                'emailMonitorId' => $client_monitor->emailMonitor?->id,
+                'paymentMonitorId' => $client_monitor->paymentMonitor?->id,
+                'mettingMonitorId' => $client_monitor->mettingMonitor?->id,
+                'whatsappMonitorId' => $client_monitor->whatsappMonitor?->id,
+                'company_name' => $client_monitor->company?->business_name,
+                'type' => $client_monitor->type,
+                'concept' => $client_monitor->concept,
+                'seller' => $client_monitor->seller->name,
+                'date' => $client_monitor->date?->isoFormat('DD MMMM YYYY, h:m A'),
+                   ];
+               });
 
         // return $client_monitors;
 
