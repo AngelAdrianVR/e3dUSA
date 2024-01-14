24b5f928b67dddd25dae8fd34e64e1737deaa280
Angel Vazquez <angelvazquez470@gmail.com>
2024-01-14 16:20:58 -0600

    load state added in suppliers products

--- resources/js/Pages/Supplier/Show.vue
+++ resources/js/Pages/Supplier/Show.vue
@@ -104,10 +104,15 @@
 
       <!-- -------------Matriz starts 3 ------------- -->
       <div v-if="tabs == 3" class="p-7">
-        <div v-if="rawMaterials?.length > 0" class="lg:grid grid-cols-2 mt-7 gap-5">
-          <SupplierProductCard v-for="product in rawMaterials" :key="product" :product="product" />
+        <div v-if="!loading">
+          <div v-if="rawMaterials?.length > 0" class="lg:grid grid-cols-2 mt-7 gap-5">
+            <SupplierProductCard v-for="product in rawMaterials" :key="product" :product="product" />
+          </div>
+            <p v-else class="text-gray-500 text-center text-sm">No hay productos registrados a este proveedor</p>
         </div>
-          <p v-else class="text-gray-500 text-center text-sm">No hay productos registrados a este proveedor</p>
+        <div v-else class="flex justify-center items-center pt-10">
+          <i class="fa-solid fa-spinner fa-spin text-4xl text-primary"></i>
+        </div>
       </div>
 
       <!-- ------------- Matriz ends 3 ------------- -->
@@ -145,6 +150,7 @@ export default {
       // currentSupplier: null,
       tabs: 1,
       showConfirmModal: false,
+      loading: false,
       rawMaterials: [],
     };
   },
@@ -206,6 +212,7 @@ export default {
     },
     async fetchSupplierItems() {
       if ( this.rawMaterials.length <= 0) { //solo hace la peticion si no se han cargado
+          this.loading = true;
             try {
               const response = await axios.get(route('raw-materials.fetch-supplier-items', {
                 raw_materials_ids: this.supplier.data.raw_materials_id.join(',')
@@ -217,6 +224,8 @@ export default {
               }
           } catch (error) {
             console.log(error);
+          } finally {
+            this.loading = false;
           }
         }
     }
