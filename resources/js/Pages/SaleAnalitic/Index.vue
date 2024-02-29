8de8b52af3dff4ad7dc66df1edceaf4d9d433e27
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-02-29 13:17:42 -0600

    11

--- resources/js/Pages/SaleAnalitic/Index.vue
+++ resources/js/Pages/SaleAnalitic/Index.vue
@@ -5,13 +5,8 @@
         <label class="text-lg">Inicio</label>
       </div>
 
-      <!-- Estadistics -->
-      <h2 class="text-primary lg:text-lg text-sm lg:mt-8 mt-6 font-bold">Estadísticas</h2>
-      <div class="lg:grid grid-cols-1 gap-10 mt-4 space-y-4 lg:space-y-0">
-        <PieChart v-for="(item, index) in pieChartOptions" :key="index" :options="item.data" :title="item.title" :icon="item.icon" />
-      </div>
       <!-- analisis de ventas -->
-      <h2 class="text-primary lg:text-lg text-sm lg:mt-20 mt-6 font-bold">Análisis de ventas</h2>
+      <h2 class="text-primary lg:text-lg text-sm lg:mt-8 mt-6 font-bold">Análisis de ventas</h2>
       <div class="flex justify-between space-x-7 lg:w-1/2">
         <div class="flex items-center space-x-4 w-2/3 lg:w-1/2">
           <el-select @change="fetchProductSalesTop" class="lg:w-1/2" v-model="familySelected" filterable
@@ -29,96 +24,100 @@
           </el-select>
         </div>
       </div>
-    </div>
-    <h2 class="text-center text-primary mt-7">Top 20 en ventas</h2>
-
-    <!-- Tabla de productos vendidos -->
-    <div v-if="!loading" class="w-10/12 h-[301px] overflow-auto mx-auto mt-3 mb-2 rounded-md">
-      <table v-if="topProducts" class="w-full h-full mx-auto text-sm">
-        <thead>
-          <tr class="text-center border-b border-primary">
-            <th class="font-bold pb-3 pl-2 text-left">
-              N° parte
-            </th>
-            <th class="font-bold pb-3 text-left">
-              Producto
-            </th>
-            <th class="font-bold pb-3 text-left">
-              Precio ant.
-            </th>
-            <th class="font-bold pb-3 text-left">
-              Precio act.
-            </th>
-            <th class="font-bold pb-3 text-left">
-              Cliente
-            </th>
-            <th class="font-bold pb-3 text-left">
-              Cantidad vendida
-            </th>
-            <th class="font-bold pb-3 text-left">
-              Total venta
-            </th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="product in topProducts" :key="product" @click="fetchProductInfo(product)"
-            class="mb-4 hover:bg-[#dfdbdba8] cursor-pointer">
-            <td class="py-2 pl-2 rounded-l-full">
-              {{ product.part_number }}
-            </td>
-            <td class="py-2">
-              <p :title="product.name" class="truncate w-40">{{ product.name }}</p>
-            </td>
-            <td class="py-2">
-              ${{ product.old_price }}
-            </td>
-            <td class="py-2">
-              ${{ product.new_price }}
-            </td>
-            <td class="py-2">
-              <p :title="product.client" class="truncate w-40">{{ product.client }}</p>
-            </td>
-            <td class="py-2">
-              {{ product.quantity_sales?.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}
-            </td>
-            <td class="py-2 rounded-r-full">
-              ${{ product.total_money?.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div v-else>
-        <p class="text-sm text-center text-gray-400">No hay Ventas registradas</p>
-      </div>
-    </div>
-
-    <!-- Estado de carga  -->
-    <div v-else class="flex justify-center items-center pt-10">
-      <i class="fa-solid fa-spinner fa-spin text-4xl text-primary"></i>
-    </div>
-
-    <!-- Analisis de producto -->
-    <div v-if="productSelected" class="rounded-lg px-4 py-3 mx-9 mt-10 text-sm">
-      <div class="w-full" v-if="!loadingCharts">
-        <div class="flex items-center space-x-9 font-bold">
-          <p>{{ productSelected.part_number }}</p>
-          <p>{{ productSelected.name }}</p>
-        </div>
-        <div class="lg:flex space-x-4 mt-5">
-          <figure class="rounded-md">
-            <img :src="productSelected.media?.original_url" class="rounded-md object-contain w-full h-28">
-          </figure>
-          <div class="lg:grid grid-cols-2 gap-4 text-center border w-[90%]">
-            <LinealChart :options="productAmountSalesMonth" title="Ventas acumuladas por mes" />
-            <!-- <ColumWithMakersChart :options="productMoneySalesMonth" title="Ventas vs Espectativas" />  -->
-            <LinealChart :options="productMoneySalesMonth" title="Ventas en pesos mexicanos $MXN por mes" />
-          </div>
+      <h2 class="text-center text-primary mt-7">Top 20 en ventas</h2>
+      <!-- Tabla de productos vendidos -->
+      <div v-if="!loading" class="w-10/12 h-[301px] overflow-auto mx-auto mt-3 mb-2 rounded-md">
+        <table v-if="topProducts" class="w-full h-full mx-auto text-sm">
+          <thead>
+            <tr class="text-center border-b border-primary">
+              <th class="font-bold pb-3 pl-2 text-left">
+                N° parte
+              </th>
+              <th class="font-bold pb-3 text-left">
+                Producto
+              </th>
+              <th class="font-bold pb-3 text-left">
+                Precio ant.
+              </th>
+              <th class="font-bold pb-3 text-left">
+                Precio act.
+              </th>
+              <th class="font-bold pb-3 text-left">
+                Cliente
+              </th>
+              <th class="font-bold pb-3 text-left">
+                Cantidad vendida
+              </th>
+              <th class="font-bold pb-3 text-left">
+                Total venta
+              </th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="product in topProducts" :key="product" @click="fetchProductInfo(product)"
+              class="mb-4 hover:bg-[#dfdbdba8] cursor-pointer">
+              <td class="py-2 pl-2 rounded-l-full">
+                {{ product.part_number }}
+              </td>
+              <td class="py-2">
+                <p :title="product.name" class="truncate w-40">{{ product.name }}</p>
+              </td>
+              <td class="py-2">
+                ${{ product.old_price }}
+              </td>
+              <td class="py-2">
+                ${{ product.new_price }}
+              </td>
+              <td class="py-2">
+                <p :title="product.client" class="truncate w-40">{{ product.client }}</p>
+              </td>
+              <td class="py-2">
+                {{ product.quantity_sales?.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}
+              </td>
+              <td class="py-2 rounded-r-full">
+                ${{ product.total_money?.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div v-else>
+          <p class="text-sm text-center text-gray-400">No hay Ventas registradas</p>
         </div>
       </div>
       <!-- Estado de carga  -->
       <div v-else class="flex justify-center items-center pt-10">
         <i class="fa-solid fa-spinner fa-spin text-4xl text-primary"></i>
+      </div>
+      <!-- Analisis de producto -->
+      <div v-if="productSelected !== null" class="rounded-lg px-4 py-3 mx-9 mt-10 text-sm">
+        <div class="w-full" v-if="!loadingCharts">
+          <div class="flex items-center space-x-9 font-bold">
+            <p>{{ productSelected.part_number }}</p>
+            <p>{{ productSelected.name }}</p>
+          </div>
+          <div class="lg:flex space-x-4 mt-5">
+            <figure class="rounded-md">
+              <img :src="productSelected.media?.original_url" class="rounded-md object-contain w-full h-28">
+            </figure>
+            <div class="lg:grid grid-cols-2 gap-4 text-center border w-[90%]">
+              <LinealChart :options="productAmountSalesMonth" title="Ventas acumuladas por mes" />
+              <!-- <ColumWithMakersChart :options="productMoneySalesMonth" title="Ventas vs Espectativas" />  -->
+              <LinealChart :options="productMoneySalesMonth" title="Ventas en pesos mexicanos $MXN por mes" />
+            </div>
+          </div>
+        </div>
+        <!-- Estado de carga  -->
+        <div v-else class="flex justify-center items-center pt-10">
+          <i class="fa-solid fa-spinner fa-spin text-4xl text-primary"></i>
+        </div>
+      </div>
+
+      <!-- Estadistics -->
+      <h2 class="text-primary lg:text-lg text-sm lg:mt-20 mt-6 font-bold">Estadísticas</h2>
+      <div class="lg:grid grid-cols-1 gap-10 mt-4 space-y-4 lg:space-y-0">
+        <PieChart v-for="(item, index) in pieChartOptions" :key="index" :options="item.data" :title="item.title"
+          :icon="item.icon" />
       </div>
     </div>
   </AppLayoutNoHeader>
