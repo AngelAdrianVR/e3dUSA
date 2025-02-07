1e420c7b866374977f0c94a739d067be0ba8429b
Angel Vazquez <angelvazquez470@gmail.com>
2025-02-07 16:25:46 -0600

    modificacion en actualizacion de precio de producto. Ya se puede cambiar tambien directamente el monto y no solo el porcentaje

--- resources/js/Pages/Quote/Create.vue
+++ resources/js/Pages/Quote/Create.vue
@@ -522,7 +522,7 @@
                         <div>
                             <InputLabel value="Precio nuevo en porcentaje*" />
                             <el-input
-                                @change="calculateNewPrice"
+                                @change="calculateNewPrice()"
                                 v-model="new_price_percentage" type="number" :max="100" :min="5" step="0.1"
                                 placeholder="Ej. 5.8%"
                                 :formatter="(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
@@ -536,7 +536,7 @@
                         </div>
                         <div>
                             <InputLabel value="Precio nuevo en moneda*" />
-                            <el-input v-model="priceForm.new_price" type="text" disabled
+                            <el-input @input="calculateNewPercentage()" v-model="priceForm.new_price" type="text"
                                 :formatter="(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
                                 :parser="(value) => value.replace(/[^\d.]/g, '')" placeholder="Ej. 30.90">
                                 <template #prepend>
@@ -846,6 +846,19 @@ export default {
             const factor = 1 + this.new_price_percentage * .01;
             // guarda el precio calculado con el porcentaje seleccionado
             this.priceForm.new_price = (factor * this.itemToUpdatePrice.pivot.new_price).toFixed(2);
+        },
+        calculateNewPercentage() {
+            if (!this.priceForm.new_price || !this.itemToUpdatePrice.pivot.new_price) {
+                this.new_price_percentage = 0;
+                return;
+            }
+
+            // Convierte los valores a número
+            const oldPrice = parseFloat(this.itemToUpdatePrice.pivot.new_price);
+            const newPrice = parseFloat(this.priceForm.new_price);
+
+            // Calcula el porcentaje de aumento
+            this.new_price_percentage = (((newPrice / oldPrice) - 1) * 100).toFixed(2);
         },
         priceChangePercentage(pivot) {
             const oldPrice = pivot.old_price;
