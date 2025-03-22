f0d7ccce291b7edd39ec3ef0f05e33fa01c285c2
Miguel Vázquez <miguelvz26.mv@gmail.com>
2025-03-21 18:47:24 -0600

    Clientes
    • Desborde de imagen sugerido al seleccionar

--- resources/js/Pages/Company/Create.vue
+++ resources/js/Pages/Company/Create.vue
@@ -448,7 +448,7 @@
             </el-select>
           </div>
           <figure v-if="selectedProductImageUrl" class="h-36 flex items-center justify-center border border-gray-400 rounded-md my-2">
-            <img :src="selectedProductImageUrl" class="object-contain">
+            <img :src="selectedProductImageUrl" class="object-contain h-36">
           </figure>
           <el-divider />
           <div class="md:text-right mt-4">
