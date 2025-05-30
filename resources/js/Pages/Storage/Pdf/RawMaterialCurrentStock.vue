a544f91169f40ff23f87f45348096a67864ba031
Miguel Vázquez <miguelvz26.mv@gmail.com>
2025-05-29 20:05:54 -0600

    Materia prima
    •cantidad max y min en reporte de inventario

--- resources/js/Pages/Storage/Pdf/RawMaterialCurrentStock.vue
+++ resources/js/Pages/Storage/Pdf/RawMaterialCurrentStock.vue
@@ -1,4 +1,5 @@
 <template>
+
     <Head :title="'Reporte pdf'" />
     <div>
         <table class="styled-table">
@@ -10,6 +11,8 @@
                     <th class="w-1/6 text-start">Número de parte</th>
                     <th class="w-1/6 text-start">Producto</th>
                     <th class="w-1/6 text-start">Cantidad actual</th>
+                    <th class="w-1/6 text-start">Cantidad mínima</th>
+                    <th class="w-1/6 text-start">Cantidad máxima</th>
                     <th class="w-1/6 text-start">Cantidad a pedir</th>
                     <th class="w-1/6 text-start">Costo por unidad</th>
                     <th class="w-1/6 text-start">$ por pedir</th>
@@ -22,6 +25,8 @@
                     <td :class="{ 'text-red': isQuantityLow(item) }">
                         {{ formatQuantity(item.quantity) }}
                     </td>
+                    <td>{{ formatQuantity(item.storageable?.min_quantity) }}</td>
+                    <td>{{ formatQuantity(item.storageable?.max_quantity) }}</td>
                     <td>{{ calculateSupplyQuantity(item) }}</td>
                     <td>${{ item.storageable?.cost }}</td>
                     <td>${{ calculateCost(item) }}</td>
@@ -62,7 +67,7 @@ export default {
             return item.quantity <= item.storageable?.min_quantity;
         },
         formatQuantity(quantity) {
-            return quantity.toFixed(2);
+            return quantity?.toFixed(2);
         },
         calculateSupplyQuantity(item) {
             return this.isQuantityLow(item) ? item.storageable?.max_quantity - item.quantity : 0;
@@ -78,67 +83,67 @@ export default {
 </script>
 
 <style>
-        body {
-            font-family: sans-serif;
-        }
+body {
+    font-family: sans-serif;
+}
 
-        .styled-table {
-            border-collapse: collapse;
-            margin: 25px 0;
-            font-size: 0.7em;
-            font-family: sans-serif;
-            width: 100%;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-        }
+.styled-table {
+    border-collapse: collapse;
+    margin: 25px 0;
+    font-size: 0.7em;
+    font-family: sans-serif;
+    width: 100%;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
 
-        .styled-table thead tr {
-            background-color: #d90537;
-            color: #ffffff;
-            text-align: left;
-        }
+.styled-table thead tr {
+    background-color: #d90537;
+    color: #ffffff;
+    text-align: left;
+}
 
-        .footer {
-            background-color: #d90537;
-            text-align: right;
-            font-weight: bold;
-            color: white;
-        }
+.footer {
+    background-color: #d90537;
+    text-align: right;
+    font-weight: bold;
+    color: white;
+}
 
-        .text-end {
-            text-align: right;
-        }
+.text-end {
+    text-align: right;
+}
 
-        .text-start {
-            text-align: left;
-        }
+.text-start {
+    text-align: left;
+}
 
-        .styled-table th,
-        .styled-table td {
-            padding: 12px 15px;
-        }
+.styled-table th,
+.styled-table td {
+    padding: 12px 15px;
+}
 
-        .styled-table tbody tr {
-            border-bottom: 1px solid #dddddd;
-        }
+.styled-table tbody tr {
+    border-bottom: 1px solid #dddddd;
+}
 
-        .styled-table tbody tr:nth-of-type(even) {
-            background-color: #f3f3f3;
-        }
+.styled-table tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+}
 
-        .styled-table tbody tr:last-of-type {
-            border-bottom: 2px solid #d90537;
-        }
+.styled-table tbody tr:last-of-type {
+    border-bottom: 2px solid #d90537;
+}
 
-        .styled-table tbody tr.active-row {
-            font-weight: bold;
-            color: #d90537;
-        }
+.styled-table tbody tr.active-row {
+    font-weight: bold;
+    color: #d90537;
+}
 
-        .text-red {
-            color: red;
-        }
+.text-red {
+    color: red;
+}
 
-        .text-orange {
-            color: orange;
-        }
-    </style>
+.text-orange {
+    color: orange;
+}
+</style>
