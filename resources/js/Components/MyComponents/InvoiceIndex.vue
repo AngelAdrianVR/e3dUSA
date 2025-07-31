93b86e59ab893ea1aa8e818a63752802837e78e8
Angel Vazquez <angelvazquez470@gmail.com>
2025-07-31 15:43:56 -0600

    se arreglaron algunos pequeños detalles

--- resources/js/Components/MyComponents/InvoiceIndex.vue
+++ resources/js/Components/MyComponents/InvoiceIndex.vue
@@ -110,7 +110,7 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                             </svg>
                                             Ver</el-dropdown-item>
-                                        <el-dropdown-item :command="'edit|' + scope.row.id">
+                                        <el-dropdown-item v-if="scope.row.status !== 'Cancelada' && scope.row.status !== 'Pagada'" :command="'edit|' + scope.row.id">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 mr-2">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                             </svg>
@@ -145,13 +145,13 @@
                                                     </svg>
                                                     <span class="text-xs">Pagada</span>
                                                 </el-dropdown-item>
-                                                <el-dropdown-item
+                                                <!-- <el-dropdown-item
                                                     :command="'changeStatus|' + scope.row.id + '|Cancelada'">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-[14px] mr-2">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                                     </svg>
                                                     <span class="text-xs">Cancelada</span>
-                                                </el-dropdown-item>
+                                                </el-dropdown-item> -->
                                             </div>
                                     </el-dropdown-menu>
                                 </template>
